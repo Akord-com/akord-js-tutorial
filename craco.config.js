33d281e449b10c24e183feefe5c3fe81b247b413
Richard Caetano <caetano@gmail.com>
2022-09-21 15:48:33 +0200

    added craco

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+module.exports = {
+    webpack: {
+        alias: {
+            react: path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+            '@mui/material': path.resolve(__dirname, './node_modules/@mui/material')
+        },
+        configure: webpackConfig => {
+            const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+                ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+            );
+
+            webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+            return webpackConfig;
+        }
+    }
+}
