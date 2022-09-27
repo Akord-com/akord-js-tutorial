ee1e6c6976a3f2dfb9143042b35f8bab992b4183
Raskovan <alexey.katalkin@gmail.com>
2022-09-26 22:18:38 -0400

    update rules

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,29 @@
 const path = require("path");
 module.exports = {
-    webpack: {
-        alias: {
-            react: path.resolve(__dirname, './node_modules/react'),
-            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-            '@mui/material': path.resolve(__dirname, './node_modules/@mui/material')
-        },
-        configure: webpackConfig => {
-            const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
-                ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
-            );
+  webpack: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+      "@mui/material": path.resolve(__dirname, "./node_modules/@mui/material")
+    },
+    configure: webpackConfig => {
+      const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+        ({ constructor }) =>
+          constructor && constructor.name === "ModuleScopePlugin"
+      );
 
-            webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
-            return webpackConfig;
+      webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+      webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+        if (rule.oneOf instanceof Array) {
+          rule.oneOf[rule.oneOf.length - 1].exclude = [
+            /\.(js|mjs|jsx|cjs|ts|tsx)$/,
+            /\.html$/,
+            /\.json$/
+          ];
         }
+        return rule;
+      });
+      return webpackConfig;
     }
-}
+  }
+};
